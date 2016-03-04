2781aa1344f3dbfa7df6ab5a400c9443c27d1ce0
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-03-04 17:44:51 -0600

    ejemplo con asyntask

--- FillListFromInternet/app/src/main/java/com/devworms/filllistfrominternet/MainActivity.java
+++ FillListFromInternet/app/src/main/java/com/devworms/filllistfrominternet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.devworms.filllistfrominternet;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,45 +9,35 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+import com.devworms.filllistfrominternet.com.devworms.utils.ImageAdapter;
+import com.devworms.filllistfrominternet.com.devworms.utils.ImageDownloader;
+
+public class MainActivity extends ListActivity implements RadioGroup.OnCheckedChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(this);
+
+        setListAdapter(new ImageAdapter());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        ImageDownloader.Mode mode = ImageDownloader.Mode.NO_ASYNC_TASK;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (checkedId == R.id.correctButton) {
+            mode = ImageDownloader.Mode.CORRECT;
+        }else if (checkedId == R.id.randomButton) {
+            mode = ImageDownloader.Mode.NO_DOWNLOADED_DRAWABLE;
         }
 
-        return super.onOptionsItemSelected(item);
+        ((ImageAdapter) getListAdapter()).getImageDownloader().setMode(mode);
     }
 }
